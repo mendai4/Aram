2c5450469b51cb824cbe0dc7a98b7eb856ba14a8
mendai4 <56698028+mendai4@users.noreply.github.com>
2025-12-05 12:20:06 +0400

    Add files via upload

---
+++ dfs bfs.py
@@ -0,0 +1,55 @@
+graph = {
+    "A": ["A1", "A2", "A3"],
+    "A1" :["B1", "B20"],
+    "B1" :["C10"],
+    "C10": [],
+    "B20" :[],
+    "A2" :["D10", "D2"],
+    "D10": [],
+    "D2": ["E10"],
+    "E10": [],
+    "A3" :["F10"],
+    "F10": [],
+}
+
+# #----------
+# #   dfs
+# #----------
+
+# visited = set()
+# count = 0
+
+# def dfs(node):
+#     global count
+#     if node not in visited:
+#         print(node)
+#         visited.add(node)
+#         count += 1
+#         for neighbor in graph[node]:
+#             dfs(neighbor)
+
+# dfs('A')
+
+# total_rooms = len(graph)
+# print(f"Visited {count} out of {total_rooms} rooms.")
+
+
+#----------
+#   bfs
+#----------
+
+visited = set()
+count = 0
+queue = ['A']
+
+while queue:
+    node = queue.pop(0)
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        count += 1
+        for neighbor in graph[node]:
+            queue.append(neighbor)
+
+total_rooms = len(graph)
+print(f"Visited {count} out of {total_rooms} rooms.")
